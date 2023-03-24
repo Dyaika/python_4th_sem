d0c882e711bf73d7d915416126abc39cc945dc22
Alexandr <razchurik@gmail.com>
2023-03-24 09:49:33 +0300

    pract 2

---
+++ ch2z1.py
@@ -0,0 +1,16 @@
+print ('Hello, world!')
+x=5+2
+my_list = [1,2]
+
+
+def f1(arg1 ='value'):
+    pass
+
+
+def foo():
+    pass
+
+if x == 6: x += 1
+x = 5; y = 10
+a = x == None
+a = x == True
---
+++ z7.py
@@ -0,0 +1,11 @@
+def main(x):
+    res = int(x, 0)
+    
+    return res
+
+
+print(main('0xd69ec'))
+print(main('0x41854a'))
+print(main('0xa9c614'))
+
+
